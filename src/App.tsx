88d5a75f62344abeba292d6423c372aad0ba3754
jbdura <bidurajohnson@gmail.com>
2023-05-12 15:46:00 +0300

    BUg 6 done

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,11 @@ export function App() {
   const { data: employees, ...employeeUtils } = useEmployees()
   const { data: paginatedTransactions, ...paginatedTransactionsUtils } = usePaginatedTransactions()
   const { data: transactionsByEmployee, ...transactionsByEmployeeUtils } = useTransactionsByEmployee()
-  const [isLoading, setIsLoading] = useState(false)
+  const [ isLoading, setIsLoading ] = useState(false)
 
   const transactions = useMemo(
     () => paginatedTransactions?.data ?? transactionsByEmployee ?? null,
-    [paginatedTransactions, transactionsByEmployee]
+    [ paginatedTransactions, transactionsByEmployee ]
   )
 
   const loadAllTransactions = useCallback(async () => {
@@ -27,21 +27,21 @@ export function App() {
     await paginatedTransactionsUtils.fetchAll()
 
     setIsLoading(false)
-  }, [employeeUtils, paginatedTransactionsUtils, transactionsByEmployeeUtils])
+  }, [ employeeUtils, paginatedTransactionsUtils, transactionsByEmployeeUtils ])
 
   const loadTransactionsByEmployee = useCallback(
     async (employeeId: string) => {
       paginatedTransactionsUtils.invalidateData()
       await transactionsByEmployeeUtils.fetchById(employeeId)
     },
-    [paginatedTransactionsUtils, transactionsByEmployeeUtils]
+    [ paginatedTransactionsUtils, transactionsByEmployeeUtils ]
   )
 
   useEffect(() => {
     if (employees === null && !employeeUtils.loading) {
       loadAllTransactions()
     }
-  }, [employeeUtils.loading, employees, loadAllTransactions])
+  }, [ employeeUtils.loading, employees, loadAllTransactions ])
 
   return (
     <Fragment>
@@ -53,7 +53,7 @@ export function App() {
         <InputSelect<Employee>
           isLoading={isLoading}
           defaultValue={EMPTY_EMPLOYEE}
-          items={employees === null ? [] : [EMPTY_EMPLOYEE, ...employees]}
+          items={employees === null ? [] : [ EMPTY_EMPLOYEE, ...employees ]}
           label="Filter by employee"
           loadingLabel="Loading employees"
           parseItem={(item) => ({
@@ -74,7 +74,8 @@ export function App() {
         <div className="RampGrid">
           <Transactions transactions={transactions} />
 
-          {transactions !== null && (
+
+          {transactions && paginatedTransactions?.nextPage && (
             <button
               className="RampButton"
               disabled={paginatedTransactionsUtils.loading}
